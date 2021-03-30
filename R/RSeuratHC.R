824a4ad65a7823632facf230cb67735feb370c46
zouzh14 <zouzh14@gmail.com>
2021-03-30 11:22:51 +0800

    Correct bug about DefaultAssay

--- R/RSeuratHC.R
+++ R/RSeuratHC.R
@@ -3,13 +3,17 @@ FindClusteringTree <- function(object, graph.type = "SNN", random.seed = 0){
   require(Seurat)
   require(Matrix)
 
+  SeuratDefaultAssay <- DefaultAssay(object)
+
   if(graph.type == "SNN"){
-    G = as.sparse(object@graphs$RNA_snn)
+    SNN_name = paste(SeuratDefaultAssay, "_snn", sep = "")
+    G = as.sparse(object@graphs[[SNN_name]])
     ClusteringTree = HGC.paris(G)
     object@graphs[["ClusteringTree"]] = ClusteringTree
     return(object)
   }else if(graph.type == "KNN"){
-    G = as.sparse(object@graphs$RNA_nn)
+    KNN_name = paste(SeuratDefaultAssay, "_nn", sep = "")
+    G = as.sparse(object@graphs[[KNN_name]])
     G = G + t(G)
     ClusteringTree = HGC.paris(G)
     object@graphs[["ClusteringTree"]] = ClusteringTree
